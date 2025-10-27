edc55182e4e60edfb4f574e86f3701d8b1f8b4d9
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-10-27 16:33:05 +0100

    fix(): resolved issue of setup metadatablock script
    The issue is that it was being ran as interactive that certain terminals
    didn't support

--- utils/metadata/upload.sh
+++ utils/metadata/upload.sh
@@ -9,7 +9,7 @@ cd "$SCRIPT_DIR" || exit 1
 
 docker cp upload-blocks.sh "$container_name":/opt/payara/
 docker cp . "$container_name":/opt/payara/
-docker exec -it "$container_name" chmod +x /opt/payara/upload-blocks.sh
-docker exec -it "$container_name" sh /opt/payara/upload-blocks.sh
+docker exec "$container_name" chmod +x /opt/payara/upload-blocks.sh
+docker exec "$container_name" sh /opt/payara/upload-blocks.sh
 
 cd "$CURRENT_DIR" || exit 1
